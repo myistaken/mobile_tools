3a4edca8764a24d6214147fc65149dfd8da9801b
Mustafa YILMAZ <myiistaken@gmail.com>
2022-09-11 21:51:53 +0300

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.flight)),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_car)),
+          ],
+        ),
+        title: Text('Mobile Tools'),
+      ),
+      body: TabBarView(
+        children: [
+          Icon(Icons.flight, size: 350),
+          Icon(Icons.directions_transit, size: 350),
+          Icon(Icons.directions_car, size: 350),
+        ],
+      ),
+    );
+  }
+}
