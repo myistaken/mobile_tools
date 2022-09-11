900c739d4fadbed1fd33ce7541efc4b8a7b9da7b
Mustafa YILMAZ <myiistaken@gmail.com>
2022-09-11 22:08:46 +0300

    fix tabs

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,23 +10,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        bottom: TabBar(
-          tabs: [
-            Tab(icon: Icon(Icons.flight)),
-            Tab(icon: Icon(Icons.directions_transit)),
-            Tab(icon: Icon(Icons.directions_car)),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.flight)),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_car)),
+            ],
+          ),
+          title: Text('Mobile Tools'),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.flight, size: 350),
+            Icon(Icons.directions_transit, size: 350),
+            Icon(Icons.directions_car, size: 350),
           ],
         ),
-        title: Text('Mobile Tools'),
-      ),
-      body: TabBarView(
-        children: [
-          Icon(Icons.flight, size: 350),
-          Icon(Icons.directions_transit, size: 350),
-          Icon(Icons.directions_car, size: 350),
-        ],
       ),
     );
   }
